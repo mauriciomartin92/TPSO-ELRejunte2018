7a2e22dc571325674a9ed02fe9aec72a8c6136f2
Gabriel Ruderman <gabriel_ruderman@hotmail.com>
2018-04-17 16:44:45 -0300

    Se crearon los proyectos de los 4 procesos del TP

---
+++ coordinador/src/coordinador.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : coordinador.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Este es el Coordinador!!!");
+	return EXIT_SUCCESS;
+}
---
+++ instancia/src/instancia.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : instancia.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Esta es una Instancia!!!");
+	return EXIT_SUCCESS;
+}
