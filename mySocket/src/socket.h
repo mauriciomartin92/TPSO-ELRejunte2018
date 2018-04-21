7ac798eb7cd7fae9af2547f0536e5d6415398111
Gabriel Ruderman <gabriel_ruderman@hotmail.com>
2018-04-21 16:00:01 -0300

    Se crea una biblioteca de Sockets interna

---
+++ mySocket/src/socket.h
@@ -0,0 +1,14 @@
+/*
+ * socket.h
+ *
+ *  Created on: 21 abr. 2018
+ *      Author: utnso
+ */
+
+#include "socket.c"
+#ifndef SOCKET_H_
+#define SOCKET_H_
+
+int conectarComoCliente();
+
+#endif /* SOCKET_H_ */
