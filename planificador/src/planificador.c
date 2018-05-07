82d8113ebdec59d05b12107a4277022d0b6c9b2c
lucasvalenzuela93 <lucasvalenzuela93@gmail.com>
2018-05-06 22:32:31 -0300

    Modifico algo del switch de imprimir_menu

--- planificador/src/planificador.c
+++ planificador/src/planificador.c
@@ -37,52 +37,52 @@ int imprimir_menu() {
 	printf("\n");
 
 	switch (seleccion) {
-	case 1:
-		printf("Pausar/Continuar ACTIVADO");
-		printf("\nGracias, se esta procesando su solicitud...\n");
-		break;
+	case '1':
+			printf("Pausar/Continuar ACTIVADO");
+			printf("\nGracias, se esta procesando su solicitud...\n");
+			break;
 
-	case 2:
-		printf("Bloquear ACTIVADO");
-		printf("\nIngrese clave: ");
-		scanf("%d", &clave);
-		printf("Ingrese ID: ");
-		scanf("%d", &id);
-		printf("Gracias, se esta procesando su solicitud...\n");
-		break;
+		case '2':
+			printf("Bloquear ACTIVADO");
+			printf("\nIngrese clave: ");
+			scanf("%d", &clave);
+			printf("Ingrese ID: ");
+			scanf("%d", &id);
+			printf("Gracias, se esta procesando su solicitud...\n");
+			break;
 
-	case 3:
-		printf("Desbloquear ACTIVADO");
-		printf("Ingrese clave: ");
-		scanf("%d", &clave);
-		printf("Gracias, se esta procesando su solicitud...\n");
-		break;
+		case '3':
+			printf("Desbloquear ACTIVADO");
+			printf("Ingrese clave: ");
+			scanf("%d", &clave);
+			printf("Gracias, se esta procesando su solicitud...\n");
+			break;
 
-	case 4:
-		printf("Listar ACTIVADO");
-		printf("Ingrese recurso: ");
-		scanf("%d", &recurso);
-		printf("Gracias, se esta procesando su solicitud...\n");
-		break;
+		case '4':
+			printf("Listar ACTIVADO");
+			printf("Ingrese recurso: ");
+			scanf("%d", &recurso);
+			printf("Gracias, se esta procesando su solicitud...\n");
+			break;
 
-	case 5:
-		printf("Kill ACTIVADO");
-		printf("Ingrese ID: ");
-		scanf("%d", &id);
-		printf("Gracias, se esta procesando su solicitud...\n");
-		break;
+		case '5':
+			printf("Kill ACTIVADO");
+			printf("Ingrese ID: ");
+			scanf("%d", &id);
+			printf("Gracias, se esta procesando su solicitud...\n");
+			break;
 
-	case 6:
-		printf("Status ACTIVADO");
-		printf("\nIngrese clave: ");
-		scanf("%d", &clave);
-		printf("Gracias, se esta procesando su solicitud...\n");
-		break;
+		case '6':
+			printf("Status ACTIVADO");
+			printf("\nIngrese clave: ");
+			scanf("%d", &clave);
+			printf("Gracias, se esta procesando su solicitud...\n");
+			break;
 
-	case 7:
-		printf("Deadlock ACTIVADO");
-		printf("\nGracias, se esta procesando su solicitud...\n");
-		break;
+		case '7':
+			printf("Deadlock ACTIVADO");
+			printf("\nGracias, se esta procesando su solicitud...\n");
+			break;
 	}
 	printf("%d\n", seleccion);
 	return seleccion;
