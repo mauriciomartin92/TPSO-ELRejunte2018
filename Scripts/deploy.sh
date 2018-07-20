1a50b4a72202e62c595fb3c901720e6311b24d45
matiasgasparini <matias_gasparini@hotmail.com>
2018-07-19 22:28:12 -0300

    meto script nuevo

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -38,7 +38,7 @@ echo "-------------------------------------------- COMPILADA -------------------
 
 echo "-------------------------------------------- INCLUYENDO BIBLIOTECA ----------------------------------------------"
 
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2018-1c-El-Rejunte/biblioteca-El-Rejunte/Debug" >> /home/utnso/.bashrc
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/workspace/tp-2018-1c-El-Rejunte/biblioteca-El-Rejunte/Debug
 
 echo "-------------------------------------------- EN PATH ----------------------------------------------" 
 
